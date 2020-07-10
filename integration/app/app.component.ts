5a46c31c3de7b36020328df79802a7066b88cb42
Sebastian Kempken <sebastian@kempken.one>
2020-07-10 18:22:57 +0200

    feat(action-provider): extend integration app with action provider

--- integration/app/app.component.ts
+++ integration/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { RootState, UpdateTitleAction } from './state';
+import { RootState, UpdateSubtitleAction, UpdateTitleAction } from './state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  @Select(RootState.tilte)
+  @Select(RootState.title)
   public title$!: Observable<string>;
+
+  @Select(RootState.subTitle)
+  public subTitle$!: Observable<string>;
 
   constructor(private store: Store) {}
 
   public changeTitle() {
     return this.store.dispatch(new UpdateTitleAction('Updated Title'));
+  }
+
+  public changeSubTitle() {
+    return this.store.dispatch(new UpdateSubtitleAction('Updated Subtitle'));
   }
 }
--- integration/app/app.module.ts
+++ integration/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxsModule, State, StateContext, Selector } from '@ngxs/store';
+import { NgxsModule } from '@ngxs/store';
 
 import { AppComponent } from './app.component';
-import { attachAction } from '@ngxs-labs/attach-action';
 import { RootState } from './state';
 
 export interface RootStateModel {
   title: string;
+  subTitle: string;
 }
 
 @NgModule({
